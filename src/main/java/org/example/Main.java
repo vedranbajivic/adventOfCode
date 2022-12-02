ab9dd61cdebee40b1cfebef490e36d32e22e6b79
vedranbajivic <vbajivic@gmail.com>
2022-12-02 17:40:23 +0100

    test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello veco!");
+        System.out.println("Hello vex!");
     }
 }
